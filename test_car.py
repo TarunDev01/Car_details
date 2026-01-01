c47f9deb7cca123a8205e4cc5fd3fc25141d7cd1
Tarun dev <devtarunmagadi@gmail.com>
2026-01-01 11:40:18 +0530

    Update test_car.py

--- test_car.py
+++ test_car.py
@@ -1,18 +1,16 @@
 
-from car import Car
+def test_full_name():
+    car = Car("Toyota", "Fortuner", 2022, 4500000)
+    assert car.full_name() == "Toyota Fortuner"
 
-def test_total_cost_basic():
-    car = Car("C101", "Tata", "Nexon", 1200000, 2)
-    assert car.total_cost() == 2400000
+def test_is_luxury_true():
+    car = Car("BMW", "X5", 2023, 8000000)
+    assert car.is_luxury() is True
 
-def test_total_cost_single_car():
-    car = Car("C102", "Honda", "City", 1000000, 1)
-    assert car.total_cost() == 1000000
+def test_is_luxury_false():
+    car = Car("Maruti", "Swift", 2021, 800000)
+    assert car.is_luxury() is False
 
-def test_total_cost_zero_quantity():
-    car = Car("C103", "Hyundai", "i20", 800000, 0)
-    assert car.total_cost() == 0
-
-def test_negative_price():
-    car = Car("C104", "Test", "ModelX", -500000, 1)
-    assert car.total_cost() == -500000
+def test_car_age():
+    car = Car("Honda", "City", 2020, 1500000)
+    assert car.car_age(2025) == 5
