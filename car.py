d224837be72c961be3e7b1617e4a91ab9c2fbb2c
Tarun dev <devtarunmagadi@gmail.com>
2026-01-01 11:46:23 +0530

    Update car.py

--- car.py
+++ car.py
@@ -9,7 +9,26 @@ class Car:
         return f"{self.brand} {self.model}"
 
     def is_luxury(self):
-        return self.price > 2000000  # price in INR
+        return self.price > 2000000  # INR
 
     def car_age(self, current_year):
         return current_year - self.year
+
+if __name__ == "__main__":
+    print("=== Car Details Input ===")
+
+    brand = input("Enter car brand: ")
+    model = input("Enter car model: ")
+    year = int(input("Enter manufacturing year: "))
+    price = int(input("Enter price (INR): "))
+
+    car = Car(brand, model, year, price)
+
+    print("\n--- Car Information ---")
+    print("Car Name:", car.full_name())
+    print("Car Age:", car.car_age(2025), "years")
+
+    if car.is_luxury():
+        print("Category: Luxury Car 🚘")
+    else:
+        print("Category: Non-Luxury Car 🚗")
